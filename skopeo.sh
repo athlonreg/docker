378091098847113c9231dd6ed40aff987c8990ac
athlonreg <15563836030@163.com>
2024-12-30 11:53:54 +0800

    actions

--- skopeo.sh
+++ skopeo.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-TOKEN=${HUAWEI_USERNAME}:${HUAWEI_TOKEN} ${REGISTRY}
+TOKEN=${HUAWEI_USERNAME}:${HUAWEI_TOKEN}
 
-skopeo login -u ${HUAWEI_USERNAME} -p ${HUAWEI_TOKEN} 
+skopeo login -u ${HUAWEI_USERNAME} -p ${HUAWEI_TOKEN} ${REGISTRY}
 
 for IMAGE in $(cat ${IMAGES_LIST_FILE} | sed '/^#/d')
 do
