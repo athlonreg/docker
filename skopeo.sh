bde87b8d547daa6b9ee0225afd0aed11460d65b0
athlonreg <15563836030@163.com>
2024-12-30 11:31:55 +0800

    skopeo image syncer

---
+++ skopeo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+for IMAGE in $(cat ${IMAGES_LIST_FILE} | sed '/^#/d')
+do
+	echo "syncing $IMAGE..."
+	IMAGE_NAME=$(echo "$IMAGE" | awk -F '/' '{print $NF}' | awk -F ':' '{print $1}')
+	IMAGE_TAG=$(echo "$IMAGE" | awk -F '/' '{print $NF}' | awk -F ':' '{print $2}')
+	SRC=docker://$IMAGE
+	DEST=docker://$REGISTRY/$REPOSITORY/$IMAGE_NAME:$IMAGE_TAG
+	echo
+	sleep 2
+	if skopeo copy --insecure-policy --src-tls-verify=false --dest-tls-verify=false --dest-creds $TOKEN --multi-arch all --override-os linux ${SRC} ${DEST}
+	then
+	  echo "Process: sync $SRC to $DEST successfully"
+	else
+	  echo "$SRC sync failed"
+	  exit 1
+	fi
+	sleep 1
+done
