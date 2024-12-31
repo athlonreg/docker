840fd672ec231f8e60493539386e6b1472dd692a
athlonreg <15563836030@163.com>
2024-12-31 11:58:19 +0800

    actions: use sync

--- skopeo.sh
+++ skopeo.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-set -ex
+CR_LIST=(
+	swr.cn-east-5.myhuaweicloud.com/tsiongchi/
+	szdk.geovisearth.com/library/
+	quay.io/taolu/
+	ghcr.io/athlonreg/
+	docker.io/canvas1996/
+	registry.cn-hangzhou.aliyuncs.com/tlhub/
+)
 
-for IMAGE in $(cat ${IMAGES_LIST_FILE} | sed '/^#/d')
+skopeo login -u cn-east-5@0HCRVNHPPP6VDOFKP9TX -p $(echo ${HUAWEI_TOKEN} | awk -F ':' '{print $2}') swr.cn-east-5.myhuaweicloud.com
+skopeo login -u 15563836030 -p $(echo ${ALIYUN_TOKEN} | awk -F ':' '{print $2}') registry.cn-hangzhou.aliyuncs.com
+skopeo login -u taolu -p $(echo ${QUAYIO_TOKEN} | awk -F ':' '{print $2}') quay.io
+skopeo login -u canvas1996 -p $(echo ${GHCR_TOKEN} | awk -F ':' '{print $2}') ghcr.io
+skopeo login -u canvas1996 -p $(echo ${DOCKER_TOKEN} | awk -F ':' '{print $2}') docker.io
+skopeo login -u canvas1996 -p $(echo ${DOCKER_TOKEN} | awk -F ':' '{print $2}') szdk.geovisearth.com
+
+for CR in ${CR_LIST[*]}
 do
-	echo "syncing $IMAGE..."
-	IMAGE_NAME=$(echo "$IMAGE" | awk -F '/' '{print $NF}' | awk -F ':' '{print $1}')
-	IMAGE_TAG=$(echo "$IMAGE" | awk -F '/' '{print $NF}' | awk -F ':' '{print $2}')
-	SRC=docker://$IMAGE
-	DEST=docker://$REGISTRY/$REPOSITORY/$IMAGE_NAME:$IMAGE_TAG
-	echo
-	sleep 2
-	if skopeo copy --dest-creds $TOKEN --multi-arch all ${SRC} ${DEST}
-	then
-	  echo "Process: sync $SRC to $DEST successfully"
-	else
-	  echo "$SRC sync failed"
-	  exit 1
-	fi
-	sleep 1
+	echo "syncing to ${CR}"
+	skopeo sync --all --retry-times=3 --keep-going --insecure-policy --src-tls-verify=false --dest-tls-verify=false --src yaml --dest docker ${IMAGES_LIST_FILE} ${CR}
 done
