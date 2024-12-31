8714577de497ec2cc9e3f01dbe6edebb2d6497b8
athlonreg <15563836030@163.com>
2024-12-31 12:05:07 +0800

    actions: use sync

--- skopeo.sh
+++ skopeo.sh
@@ -9,15 +9,15 @@ CR_LIST=(
 	registry.cn-hangzhou.aliyuncs.com/tlhub/
 )
 
-skopeo login -u cn-east-5@0HCRVNHPPP6VDOFKP9TX -p $(echo ${HUAWEI_TOKEN} | awk -F ':' '{print $2}') swr.cn-east-5.myhuaweicloud.com
-skopeo login -u 15563836030 -p $(echo ${ALIYUN_TOKEN} | awk -F ':' '{print $2}') registry.cn-hangzhou.aliyuncs.com
-skopeo login -u taolu -p $(echo ${QUAYIO_TOKEN} | awk -F ':' '{print $2}') quay.io
-skopeo login -u canvas1996 -p $(echo ${GHCR_TOKEN} | awk -F ':' '{print $2}') ghcr.io
-skopeo login -u canvas1996 -p $(echo ${DOCKER_TOKEN} | awk -F ':' '{print $2}') docker.io
-skopeo login -u canvas1996 -p $(echo ${DOCKER_TOKEN} | awk -F ':' '{print $2}') szdk.geovisearth.com
+skopeo login -u $(echo ${HUAWEI_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${HUAWEI_TOKEN} | awk -F ':' '{print $2}') swr.cn-east-5.myhuaweicloud.com
+skopeo login -u $(echo ${ALIYUN_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${ALIYUN_TOKEN} | awk -F ':' '{print $2}') registry.cn-hangzhou.aliyuncs.com
+skopeo login -u $(echo ${QUAYIO_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${QUAYIO_TOKEN} | awk -F ':' '{print $2}') quay.io
+skopeo login -u $(echo ${GHCR_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${GHCR_TOKEN} | awk -F ':' '{print $2}') ghcr.io
+skopeo login -u $(echo ${DOCKER_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${DOCKER_TOKEN} | awk -F ':' '{print $2}') docker.io
+skopeo login -u $(echo ${EARTH_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${EARTH_TOKEN} | awk -F ':' '{print $2}') szdk.geovisearth.com
 
 for CR in ${CR_LIST[*]}
 do
 	echo "syncing to ${CR}"
-	skopeo sync --all --retry-times=3 --keep-going --insecure-policy --src-tls-verify=false --dest-tls-verify=false --src yaml --dest docker ${IMAGES_LIST_FILE} ${CR}
+	skopeo sync --all --retry-times=3 --keep-going --insecure-policy --src-no-creds --src-tls-verify=false --dest-tls-verify=false --src yaml --dest docker ${IMAGES_LIST_FILE} ${CR}
 done
