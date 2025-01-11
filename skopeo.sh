6b5f788dbfc1180dd33b211a7d2d5789977f5743
athlonreg <15563836030@163.com>
2025-01-11 22:28:08 +0800

    Remove: Aliyun CR

--- skopeo.sh
+++ skopeo.sh
@@ -5,11 +5,10 @@ CR_LIST=(
 	szdk.geovisearth.com/library/
 	quay.io/taolu/
 	ghcr.io/athlonreg/
-	registry.cn-hangzhou.aliyuncs.com/tlhub/
 )
 
 skopeo login -u $(echo ${HUAWEI_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${HUAWEI_TOKEN} | awk -F ':' '{print $2}') swr.cn-east-5.myhuaweicloud.com
-skopeo login -u $(echo ${ALIYUN_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${ALIYUN_TOKEN} | awk -F ':' '{print $2}') registry.cn-hangzhou.aliyuncs.com
+#skopeo login -u $(echo ${ALIYUN_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${ALIYUN_TOKEN} | awk -F ':' '{print $2}') registry.cn-hangzhou.aliyuncs.com
 skopeo login -u $(echo ${QUAYIO_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${QUAYIO_TOKEN} | awk -F ':' '{print $2}') quay.io
 skopeo login -u $(echo ${GHCR_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${GHCR_TOKEN} | awk -F ':' '{print $2}') ghcr.io
 skopeo login -u $(echo ${EARTH_TOKEN} | awk -F ':' '{print $1}') -p $(echo ${EARTH_TOKEN} | awk -F ':' '{print $2}') szdk.geovisearth.com
